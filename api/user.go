fd234d40eec8b10e282285e1a369ee0a13a39e76
Avery King <kavery086@gmail.com>
2022-10-29 11:00:34 -0500

    unique username error fixed

--- api/user.go
+++ api/user.go
@@ -1,9 +1,9 @@
 package api
 
 import (
-	"errors"
 	db "github.com/AveryKing/tasks/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 	"net/http"
 )
 
@@ -26,7 +26,17 @@ func (server *Server) createUser(ctx *gin.Context) {
 
 	user, err := server.queries.CreateUser(ctx, arg)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, errors.New("There was an error creating your account"+err.Error()))
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, gin.H{
+					"error": "Username '" + req.Username + "' is already taken",
+				})
+				break
+			}
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, err)
 		return
 	}
 	ctx.JSON(http.StatusOK, user)
