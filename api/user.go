786b92e56365e5f09a4942173935f1abbaca51cd
Avery King <kavery086@gmail.com>
2022-10-28 23:26:40 -0500

    create user api implemented

--- api/user.go
+++ api/user.go
@@ -1,9 +1,35 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	db "github.com/AveryKing/tasks/db/sqlc"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type createUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+}
 
 func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+		return
+	}
 
+	arg := db.CreateUserParams{
+		Username: req.Username,
+		Password: req.Password,
+	}
+
+	user, err := server.queries.CreateUser(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errors.New("There was an error creating your account"+err.Error()))
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
 }
 
 func (server *Server) getUser(ctx *gin.Context) {
